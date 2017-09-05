9307e893821af82e52c39650ac1a66ee9be0d3d6
DoctorWhoof <DoctorWhoof@users.noreply.github.com>
2017-09-05 13:25:11 -0700

    Removed unnecessary lines

--- countlines.sh
+++ countlines.sh
@@ -4,12 +4,10 @@
 # Use: countlines path pattern excludepattern
 
 # Example: "countlines mySourceCode *.monkey2 example"
-#this will find all files that match *.monkey2, but exclude files with "example" in name
+#this will find all files in the "mySourceCode" folder that match *.monkey2, but exclude files with "example" in name
 
 if [ $3 ]; then
 	find -L $1 -type f -name $2 ! -path "*$3*" | xargs wc -l
-	exit 1
 else
 	find -L $1 -type f -name $2 | xargs wc -l
-	exit 1
 fi
